40d7f7df8a899061e6a069c5b2ad3c07316649b8
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2023-10-19 19:01:58 +0600

    mode toggle on nav

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,6 +4,7 @@ import { marketingConfig } from "@/config/marketing"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
+import { ModeToggle } from "@/components/mode-toggle"
 import { SiteFooter } from "@/components/site-footer"
 
 interface MarketingLayoutProps {
@@ -14,21 +15,38 @@ export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex flex-col min-h-screen">
       <header className="container z-40 bg-background">
-        <div className="flex h-20 items-center justify-between py-6">
+        <div className="flex items-center justify-between h-20 py-6">
           <MainNav items={marketingConfig.mainNav} />
-          <nav>
-            <Link
-              href="/login"
-              className={cn(
-                buttonVariants({ variant: "secondary", size: "sm" }),
-                "px-4"
-              )}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              borderColor: "black",
+              alignItems: "center", // Ensures vertical alignment
+            }}
+          >
+            <ModeToggle />
+
+            <nav
+              style={{
+                borderColor: "red",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
-              Login
-            </Link>
-          </nav>
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4"
+                )}
+              >
+                Login
+              </Link>
+            </nav>
+          </div>
         </div>
       </header>
       <main className="flex-1">{children}</main>
