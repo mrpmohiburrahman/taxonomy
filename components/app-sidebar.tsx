2910637bda1c22083c7fd7f826659ad09c0e431b
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-12-04 10:18:37 +0600

    side bar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,200 @@
+import * as React from "react"
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+// import { VersionSwitcher } from "@/components/version-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { SearchForm } from "@/components/search-form"
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Getting Started",
+      url: "#",
+      items: [
+        {
+          title: "Installation",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Project Structure",
+          url: "#",
+          icon: Home,
+        },
+      ],
+    },
+    {
+      title: "Building Your Application",
+      url: "#",
+      items: [
+        {
+          title: "Routing",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Data Fetching",
+          url: "#",
+          isActive: true,
+          icon: Home,
+        },
+        {
+          title: "Rendering",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Caching",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Styling",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Optimizing",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Configuring",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Testing",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Authentication",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Deploying",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Upgrading",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Examples",
+          url: "#",
+          icon: Home,
+        },
+      ],
+    },
+    {
+      title: "API Reference",
+      url: "#",
+      items: [
+        {
+          title: "Components",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "File Conventions",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Functions",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "next.config.js Options",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "CLI",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Edge Runtime",
+          url: "#",
+          icon: Home,
+        },
+      ],
+    },
+    {
+      title: "Architecture",
+      url: "#",
+      items: [
+        {
+          title: "Accessibility",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Fast Refresh",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Next.js Compiler",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Supported Browsers",
+          url: "#",
+          icon: Home,
+        },
+        {
+          title: "Turbopack",
+          url: "#",
+          icon: Home,
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild isActive={item.isActive}>
+                      <a href={item.url}>{item.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   darkMode: ["class"],
   theme: {
     container: {
-      center: true,
+      center: "true",
       padding: "2rem",
       screens: {
         "2xl": "1400px",
@@ -52,10 +52,27 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
       borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
@@ -64,12 +81,20 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
       },
       animation: {
@@ -78,5 +103,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar"), // Add the scrollbar plugin
+  ],
 }
