722d7beff3cb100d0dd55049d0bed6bd8b4d2214
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-12-15 16:38:28 +0600

    update site config

--- config/site.ts
+++ config/site.ts
@@ -1,14 +1,8 @@
-// config/site.ts
-import { SiteConfig } from "types"
+import type { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
-  name: "Taxonomy",
-  description:
-    "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://tx.shadcn.com",
-  ogImage: "https://tx.shadcn.com/og.jpg",
-  links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/taxonomy",
-  },
+  name: "360 Car Seat Reviews",
+  description: "Find the best car seats for the little one.",
+  url: "360carseatreviews.com",
+  ogImage: "https://360carseatreviews.com/og.jpg",
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,14 @@
 // types/index.d.ts
-import type { User } from "@prisma/client"
-import type { Icon } from "lucide-react"
+import type { User } from "@prisma/client";
+import type { Icon } from "lucide-react";
 
-import type { Icons } from "@/components/icons"
+
+
+import type { Icons } from "@/components/icons";
+
+
+
+
 
 export type NavItem = {
   title: string
@@ -33,11 +39,17 @@ export type SiteConfig = {
   description: string
   url: string
   ogImage: string
-  links: {
-    twitter: string
-    github: string
-  }
 }
+// export type SiteConfig = {
+//   name: string
+//   description: string
+//   url: string
+//   ogImage: string
+//   links: {
+//     twitter: string
+//     github: string
+//   }
+// }
 
 export type DocsConfig = {
   mainNav: MainNavItem[]
